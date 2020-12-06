b4d4533eb20ac3d794168d9d9712a6213e3a2494
Sumin Hong <sumiiniii@ajou.ac.kr>
2020-12-07 00:47:39 +0900

    수정 및 삭제시 팝업창 띄우기

--- ilovepet/backend/app.js
+++ ilovepet/backend/app.js
@@ -527,7 +527,6 @@ app.post('/Addprotectcomment',function(req,res,next){
 
 
 
-//============================================================
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
